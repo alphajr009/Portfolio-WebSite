3a708f819a4b7d89fff3d901df3ce43a9e8bf405
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-07-21 02:47:35 +0530

    Final Commit

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,7 @@ import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { github } from "../assets";
+import { github, live } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
@@ -15,6 +15,7 @@ const ProjectCard = ({
   tags,
   image,
   source_code_link,
+  live_link,
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
@@ -40,6 +41,16 @@ const ProjectCard = ({
             >
               <img
                 src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
+            <div
+              onClick={() => window.open(live_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer ml-3'
+            >
+              <img
+                src={live}
                 alt='source code'
                 className='w-1/2 h-1/2 object-contain'
               />
